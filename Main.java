6b3da707137a295638b11546f8b4f2817b9bb62d
nguyen dinh bao <bbaaoob52@gmail.com>
2024-02-29 14:49:04 +0700

    first cm

---
+++ Main.java
@@ -0,0 +1,9 @@
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        int d;
+//        student bao = new student(a="bao",b="bao1",c="bao2");
+//        bao.print();
+//    }
+//}
---
+++ student.java
@@ -0,0 +1,18 @@
+public class student {
+    private String id;
+    private String name;
+    private String add;
+
+    public String getName()
+    {
+        return  name;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public static void main(String[] args){
+        System.out.println("stop");
+    }
+
+}
